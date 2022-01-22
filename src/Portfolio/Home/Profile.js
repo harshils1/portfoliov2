6ca4bfc36c5bf662d9cfced75e7bd9fb18095ecf
Harshil Shah <h66shah@uwaterloo.ca>
2022-01-22 17:33:54 -0500

    first draft on personal website (includes navbar and front page)

---
+++ src/Portfolio/Home/Profile.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Typical from 'react-typical';
+import './Profile.css';
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+            <a href="https://www.instagram.com/__harshilshah/?hl=en">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/harshils1/">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/harshils1">
+              <i className="fa fa-github"></i>
+            </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              hey, i'm a
+            </span>
+          </div>
+          <div className='profile-details-role'>
+              <span className="primary-text">
+                <h1>
+                    <Typical
+                    loop ={Infinity}
+                    steps={[
+                        "tech enthusiast", 1000,
+                        "software developer", 1000,
+                        "designer", 1000,                    
+                      ]}
+                    />
+                </h1>
+                <span className="profile-role-tagline">
+                  and this is a place where you will get to know everything about me!
+                </span>
+              </span>
+          </div>
+          <div className="profile-options">
+            <a href="resume.pdf" download="resume.pdf">  
+                    <button className='btn highlighted-btn'>Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className='profile-picture'>
+          <div className='profile-picture-background'></div>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
---
+++ src/Portfolio/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projects',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Me',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
