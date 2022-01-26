2b4e21a76b0e66cab2e4e06f8e5b104d57d71716
Harshil Shah <h66shah@uwaterloo.ca>
2022-01-26 11:16:12 -0500

    made changes to projects

--- src/Portfolio/Projects/Projects.js
+++ src/Portfolio/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Projects.css";
 import { Card, Button } from "react-bootstrap";
 
@@ -27,23 +27,59 @@ export default function Projects() {
     },
   ];
 
+  const cardInfo2 = [
+    {
+      image: ""/*"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png"*/,
+      title: "More Coming Soon...",
+      text: "Gihub Below",
+      link: "https://github.com/harshils1/",
+      className: "1"
+    },
+    {
+      image: ""/*"https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/8a/d44cb0e61c11e58ef2e3befd7c195d/websiteweekend.png?auto=format%2Ccompress&dpr=1"*/,
+      title: "More Coming Soon...",
+      text: "Gihub Below",
+      link: "https://github.com/harshils1/",
+      className: "id2"
+    },
+    {
+      image: ""/*"https://www.acurite.com/media/catalog/product/cache/c1e9ab0486932534a7d741d10c346ab0/0/1/01535--hi-res_2021.jpg"*/,
+      title: "More Coming Soon...",
+      text: "Gihub Below",
+      link: "https://github.com/harshils1/",
+      className: "3"
+    },
+  ];
+
+
   const renderCard = (card) => {
     return (
-      <Card style={{ width: "20rem" , background: "black"}} className='card' >
+      <Card style={{ width: "20rem" , background: "black", border: "2px solid white"}} className='card' >
         <Card.Img variant="top" src={card.image} />
         <Card.Body>
           <Card.Title style={{color: "white"}}>{card.title}</Card.Title>
           <Card.Text style={{color: "white"}}>{card.text}</Card.Text>
-          <Button variant="primary" className="card-button"><a className="btn-ins" href={card.link} style={{color: "white", decoration: "none"}}>View Here</a></Button>
+          <Button variant="primary" className="card-button"><a className="btn-ins" target='__blank' href={card.link} style={{color: "white", decoration: "none"}}>View Here</a></Button>
         </Card.Body>
       </Card>
     );
   };
 
+  const [version, setVersion] = useState(cardInfo.map(renderCard));
+
+
   return (
     <div className="project-container">
       <div className="project-parent">
-        <div className="project-details">{cardInfo.map(renderCard)}</div>
+        <div className="project-details">
+            <div>
+                <h1 style={{color: 'white'}} className="up" onClick={() => setVersion(cardInfo.map(renderCard))}>▲</h1>
+            </div>
+            {version}
+            <div>
+                <h1 style={{color: 'white'}} className="down" onClick={() => setVersion(cardInfo2.map(renderCard))}>▼</h1>
+            </div>
+        </div>
       </div>
     </div>
   );
