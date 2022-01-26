8c0f5e82570675b8cab9558e216babda26806988
Harshil Shah <h66shah@uwaterloo.ca>
2022-01-25 21:40:33 -0500

    v2 of portfolio

--- src/Portfolio/Projects/Projects.js
+++ src/Portfolio/Projects/Projects.js
@@ -5,33 +5,36 @@ import { Card, Button } from "react-bootstrap";
 export default function Projects() {
   const cardInfo = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png",
+      image: ""/*"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png"*/,
       title: "Self-Solving Sudoku",
-      text: "Coming Soon",
-      link: "https://github.com/harshils1/Sudoku"
+      text: "Java",
+      link: "https://github.com/harshils1/Sudoku",
+      className: "1"
     },
     {
-      image: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/8a/d44cb0e61c11e58ef2e3befd7c195d/websiteweekend.png?auto=format%2Ccompress&dpr=1",
+      image: ""/*"https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/8a/d44cb0e61c11e58ef2e3befd7c195d/websiteweekend.png?auto=format%2Ccompress&dpr=1"*/,
       title: "Portfolio Web Application",
-      text: "Coming Soon",
-      link: "https://github.com/harshils1/portfoliov2"
+      text: "React",
+      link: "https://github.com/harshils1/portfoliov2",
+      className: "id2"
     },
     {
-      image: "https://www.acurite.com/media/catalog/product/cache/c1e9ab0486932534a7d741d10c346ab0/0/1/01535--hi-res_2021.jpg",
+      image: ""/*"https://www.acurite.com/media/catalog/product/cache/c1e9ab0486932534a7d741d10c346ab0/0/1/01535--hi-res_2021.jpg"*/,
       title: "IoT Weather Station",
-      text: "Coming Soon",
-      link: "https://github.com/harshils1/IoT-Weather-Station"
+      text: "Arduino",
+      link: "https://github.com/harshils1/IoT-Weather-Station",
+      className: "3"
     },
   ];
 
-  const renderCard = (card, index) => {
+  const renderCard = (card) => {
     return (
-      <Card style={{ width: "20rem" , background: "black"}} key={index}>
+      <Card style={{ width: "20rem" , background: "black"}} className='card' >
         <Card.Img variant="top" src={card.image} />
         <Card.Body>
           <Card.Title style={{color: "white"}}>{card.title}</Card.Title>
           <Card.Text style={{color: "white"}}>{card.text}</Card.Text>
-          <Button variant="primary"><a href={card.link} style={{color: "white", decoration: "none"}}>View Here</a></Button>
+          <Button variant="primary" className="card-button"><a className="btn-ins" href={card.link} style={{color: "white", decoration: "none"}}>View Here</a></Button>
         </Card.Body>
       </Card>
     );
