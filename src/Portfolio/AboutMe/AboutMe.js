7c974a96de7fd0b12ce6785e075b623a7df4a4fc
Harshil Shah <h66shah@uwaterloo.ca>
2022-01-27 17:22:12 -0500

    added to about me page

--- src/Portfolio/AboutMe/AboutMe.js
+++ src/Portfolio/AboutMe/AboutMe.js
@@ -8,19 +8,58 @@ export default function AboutMe() {
         <div className="about-details">
           <div className="aboutme">
             <h2>Who Am I?</h2>
-            <p>Some text..</p>
+            <p className="paragraph1">
+              Hey everyone, this is Harshil! I am currently a first-year
+              computer engineering student at University of Waterloo, presently
+              in my co-op term interning as a Junior Data Engineer at
+              Infrastructure Canada! I am passionate about programming, music,
+              napping, and playing basketball.
+            </p>
           </div>
-          <div className="vl1">
-          </div>
+          <div className="vl1"></div>
           <div className="aboutme">
             <h2>Experience</h2>
-            <p>Some text..</p>
+            <p className="paragraph">
+              Junior Data Engineer | 2022
+              <br></br>
+              <br></br>
+              <p className="smallp">
+                As a junior data engineer, I am leveraging an Oracle Stack to
+                develop the enterprise data warehouse, ETL pipelines, relational
+                database models, and more!
+              </p>
+              Mobile Application Developer | 2021
+              <br></br>
+              <br></br>
+              <p className="smallp">
+                As a mobile application developer, I used React Native to help
+                develop a cross-platform mobile application. Additionally, I
+                extracted data from the company's REST API in JSON format to
+                increase the versatility of the application.
+              </p>
+            </p>
           </div>
-          <div className="vl2">
-          </div>
+          <div className="vl2"></div>
           <div className="aboutme">
             <h2>Awards</h2>
-            <p>Some text..</p>
+            <p className="paragraph">
+              $1500 Ministry of Education Grant 
+              <br></br>
+              <br></br>
+              Capt. Philip Chao Athletic Award
+              <br></br> 
+              <br></br>
+              Duke of Edinburgh (Silver) 
+              <br></br>
+              <br></br>
+              Certificate of Distinction (Fermat) 
+              <br></br>
+              <br></br>
+              5 years Service Medal in Cadets
+              <br></br>
+              <br></br>
+              Certificate of Distinction (Euclid) 
+            </p>
           </div>
         </div>
       </div>
--- src/Portfolio/Navbar/Navbar.js
+++ src/Portfolio/Navbar/Navbar.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import {MenuItems} from './MenuItems';
-import  './Navbar.css';
+import React from "react";
+import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
 
 class Navbar extends React.Component {
-    
-    constructor() {
-        super()
-        this.state = {
-            clicked: false
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      clicked: false,
+    };
+  }
 
-    handleClick() {
-        this.setState({
-            clicked: !this.state.clicked
-        })
-    }
+  handleClick() {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  }
 
+  renderNavbar(item) {
+    return (
+      <li>
+        <a className={item.cName} href={item.url}>
+          {item.title}
+        </a>
+      </li>
+    );
+  }     
 
-    render() {
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo"> <a href="./">harshil s.</a> </h1>
-                <div className='menu-icon' onClick={() => this.handleClick()}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className ={this.state.clicked ? 'nav-menu-active' : 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li key = {index}><a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}                    
-                </ul>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">
+          {" "}
+          <a href="./">harshil s.</a>{" "}
+        </h1>
+        <div className="menu-icon" onClick={() => this.handleClick()}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu-active" : "nav-menu"}>
+          {MenuItems.map(this.renderNavbar)}
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
