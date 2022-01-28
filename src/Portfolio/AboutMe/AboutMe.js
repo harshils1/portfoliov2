d9b225af7de25b256d387926746867b7cde6f78d
Harshil Shah <h66shah@uwaterloo.ca>
2022-01-27 22:25:16 -0500

    added changes to about me

--- src/Portfolio/AboutMe/AboutMe.js
+++ src/Portfolio/AboutMe/AboutMe.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AboutMe.css";
 
 export default function AboutMe() {
+
+  const [version, setVersion] = useState(true);
+  const [version2, setVersion2] = useState(true);
+  const [version3, setVersion3] = useState(true);
+
   return (
     <div className="about-container">
       <div className="about-parent">
         <div className="about-details">
           <div className="aboutme">
-            <h2>Who Am I?</h2>
+            <div className={version ? "square1" : "square-none"} onClick={() => setVersion(false)}>
+              <h2 className='headers'>Who Am I?</h2>
+              </div>
             <p className="paragraph1">
               Hey everyone, this is Harshil! I am currently a first-year
               computer engineering student at University of Waterloo, presently
@@ -18,7 +25,9 @@ export default function AboutMe() {
           </div>
           <div className="vl1"></div>
           <div className="aboutme">
-            <h2>Experience</h2>
+            <div className={version2 ? "square2" : "square-none"} onClick={() => setVersion2(false)}>
+            <h2 className="headers">Experience</h2>
+            </div>
             <p className="paragraph">
               Junior Data Engineer | 2022
               <br></br>
@@ -41,24 +50,25 @@ export default function AboutMe() {
           </div>
           <div className="vl2"></div>
           <div className="aboutme">
-            <h2>Awards</h2>
+            <div className={version3 ? "square3" : "square-none"} onClick={() => setVersion3(false)}>
+            <h2 className="headers">Awards</h2>
+            </div>
             <p className="paragraph">
-              $1500 Ministry of Education Grant 
+              $1500 Ministry of Education Grant
               <br></br>
               <br></br>
               Capt. Philip Chao Athletic Award
-              <br></br> 
-              <br></br>
-              Duke of Edinburgh (Silver) 
               <br></br>
               <br></br>
-              Certificate of Distinction (Fermat) 
+              Duke of Edinburgh (Silver)
               <br></br>
               <br></br>
-              5 years Service Medal in Cadets
+              Certificate of Distinction (Fermat)
+              <br></br>
+              <br></br>5 years Service Medal in Cadets
               <br></br>
               <br></br>
-              Certificate of Distinction (Euclid) 
+              Certificate of Distinction (Euclid)
             </p>
           </div>
         </div>
